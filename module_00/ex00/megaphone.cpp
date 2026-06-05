281e76d94839ff0f489b8bb0b737a97080cdf298
Jenna Mustajärvi <jemustaj@student.42porto.com>
2026-06-05 18:39:56 +0100

    update makefile to set o files

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -5,22 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jemustaj <jemustaj@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/03 15:45:34 by jemustaj          #+#    #+#             */
-/*   Updated: 2026/06/03 18:01:58 by jemustaj         ###   ########.fr       */
+/*   Created: 2026/06/05 13:51:47 by jemustaj          #+#    #+#             */
+/*   Updated: 2026/06/05 18:39:28 by jemustaj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-
-void	clean();
+#include <iostream.h>
 
 int	main(void)
 {
-	std::cout << "Hello\n";
-	clean();
-}
-
-void	clean()
-{
-	std::cout << "Hello\n";
+	std::
 }
